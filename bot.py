ed54a9215e8a76814ec1b59d2d16bd171e9fd090
Ksenia Berdugina <kseniaberdugina918@gmail.com>
2024-06-23 15:05:55 +0300

    Implement logging

--- bot.py
+++ bot.py
@@ -1,12 +1,34 @@
+import asyncio
+import logging
+
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+
 from config_data.config import Config, load_config
 
 
-config: Config = load_config()
-bot = Bot(token=config.tg_bot.token)
-dp = Dispatcher()
+logger = logging.getLogger(__name__)
 
+
+async def main() -> None:
+    logging.basicConfig(level=logging.INFO,
+                        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+                               '[%(asctime)s] - %(name)s - %(message)s')
+
+    logger.info('Starting bot')
+
+    config: Config = load_config()
+
+    bot = Bot(
+        token=config.tg_bot.token,
+        default=DefaultBotProperties(parse_mode='HTML')
+    )
+
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    dp.run_polling(bot)
+    asyncio.run(main())
 
