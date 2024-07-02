e8da292c23a0a72817ed5343a5704624b888027a
Ksenia Berdugina <kseniaberdugina918@gmail.com>
2024-07-02 23:12:03 +0300

    Implement CianParser class for scraping Cian.ru - Create the CianParser class to handle fetching and parsing listings from Cian.ru - Implement the get_links method to retrieve the URLs of property listings from a given page - Use the httpx library for making asynchronous HTTP requests - Utilize the BeautifulSoup library for parsing the HTML response - Integrate the fake_useragent and random modules to randomize the user agent and proxy for each request - Add basic error handling to log any exceptions that occur during the scraping process

---
+++ cian/parser.py
@@ -0,0 +1,43 @@
+import logging
+
+import httpx
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from random import choice
+
+from cian.proxies import proxy_list
+
+logger = logging.getLogger(__name__)
+
+
+class CianParser:
+    def __init__(self, url):
+        self.url = url
+
+    @staticmethod
+    def _get_headers_and_proxies():
+        headers = {'User-Agent': UserAgent().random}
+        proxies = {'http://': choice(proxy_list)}
+        return headers, proxies
+
+    async def get_links(self):
+        headers, proxies = self._get_headers_and_proxies()
+        async with httpx.AsyncClient(headers=headers, proxies=proxies) as client:
+            response = await client.get(url=self.url)
+            links = []
+            try:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                data = soup.find('div', class_='_93444fe79c--wrapper--W0WqH').find_all('div')
+                for d in data:
+                    result = d.find('article', class_='_93444fe79c--container--Povoi _93444fe79c--cont--OzgVc')
+                    if result:
+                        link = result.find('a', class_='_93444fe79c--media--9P6wN').get('href')
+                        links.append(link)
+            except Exception as e:
+                logger.error(f'Error getting links: {e}')
+
+            return links
+
+
+
+
